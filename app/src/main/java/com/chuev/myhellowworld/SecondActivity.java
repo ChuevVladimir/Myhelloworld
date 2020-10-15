331fe05ed06496caef26ffb67934d8ea9d2b3395
ChuevVladimir <trashbox96@mail.ru>
2020-10-15 20:28:09 +0700

    Add second activity

---
+++ app/src/main/java/com/chuev/myhellowworld/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.chuev.myhellowworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        int cost_id= getIntent().getIntExtra("COST_ID",0);
+        Log.e("TAG","COST_ID = "+cost_id);
+    }
+}
